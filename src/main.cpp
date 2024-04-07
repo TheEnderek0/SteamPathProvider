d9945370af80e76ab0e52ad5999ba83488bd8ce3
Trico Everfire <bobbys@live.nl>
2024-04-07 21:14:24 +0200

    Fix missing includes.

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	if (provider.BIsAppInstalled(appid)) {
-		string path = "";
+		string path;
 		provider.GetAppInstallDir(appid, path, 0);
 		cout << path;
 		return 0;
